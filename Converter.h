706dd86202d0f7938ae9206d05bee76572092fe0
Gleb <gleb.abyazov@mail.ru>
2021-12-04 22:28:41 +0600

    gleb 6

--- Converter.h
+++ Converter.h
@@ -1,13 +1,14 @@
 #include "Image.h"
-
+typedef unsigned int uint;
 class Converter{
 public:
 	static Image toGRAY(const Image& im){
 		Image im_gr(im.width, im.height);
 		for(auto p:im.pixels)
 		{
-			unsigned char md=(p.getColor().R+p.getColor().G+p.getColor().B)/3;
-			Color gr(md,md,md,p.getColor().A);
+			auto c=p.getColor();
+			unsigned char md=(c.R+uint(c.G)+uint(c.B))/3;
+			Color gr(md,md,md,c.A);
 			im_gr.setPixel(gr,p.getX(), p.getY());
 		}
 		return im_gr;
